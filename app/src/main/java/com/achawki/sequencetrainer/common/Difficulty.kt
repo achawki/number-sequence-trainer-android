aa577cb246bd3a2019dd6c4f8e4ff543cab4db05
Amin Chawki <amin.chawki@devmino.cloud>
2024-04-28 22:48:40 +0200

    Fix values() usage

--- app/src/main/java/com/achawki/sequencetrainer/common/Difficulty.kt
+++ app/src/main/java/com/achawki/sequencetrainer/common/Difficulty.kt
@@ -4,7 +4,7 @@ enum class Difficulty(val label: Int) {
     EASY(1), MEDIUM(2), HARD(3);
 
     companion object {
-        private val labelMapping = values().associateBy(Difficulty::label)
+        private val labelMapping = entries.associateBy(Difficulty::label)
         fun fromLabel(label: Int): Difficulty = labelMapping.getOrElse(label) { EASY }
     }
 }
